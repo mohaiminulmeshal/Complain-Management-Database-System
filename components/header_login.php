9af9fe2043fcc09619b18c7c18f7f841c87b31b7
Mohaiminul Islam Meshal <65761833+mohaiminulmeshal@users.noreply.github.com>
2021-08-14 17:36:55 +0600

    Add files via upload

---
+++ components/header_login.php
@@ -0,0 +1,52 @@
+
+<?php
+		require 'dbconfig/config.php';
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>User Complaints</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="style2.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="css/style.css">
+  <script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+  </script>
+</head>
+<body style="background-color:#FFA500">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+        	<img src="image/cms.png" width="35" height="45" class="d-inline-block align-top" alt="">
+        <b>COMPLAINT MANAGEMENT SYSTEM</b>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto ">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php"> HOME </a>
+            </li>
+            
+               <li class="nav-item">
+              <a class="nav-link active" href="userlogin.php"> USER LOGIN </a>
+            </li>
+
+                 <li class="nav-item">
+              <a class="nav-link active" href="adminlogin.php"> ADMIN LOGIN </a>
+            </li>
+
+
+
+          </ul>
+        </div>
+      </div>
+    </nav>
